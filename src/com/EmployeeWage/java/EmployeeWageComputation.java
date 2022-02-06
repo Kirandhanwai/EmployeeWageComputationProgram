b50017aae8ee0854feebf79b75057e7f17e0f1b2
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-06 23:22:38 +0530

    Added : Employee is FullTime or PartTime by using Switch Case Statement

--- src/com/EmployeeWage/java/EmployeeWageComputation.java
+++ src/com/EmployeeWage/java/EmployeeWageComputation.java
@@ -1,20 +1,32 @@
 package com.EmployeeWage.java;
 
 public class EmployeeWageComputation {
-	public static void main(String[] args) {
-		int is_full_time = 1;
-		int is_part_time = 2;
-		int Employee_wage_per_Hour = 20;
-		int empHrs = 0;
-		int empwage = 0;
-		double empcheck = Math.floor(Math.random() * 100) % 3;
-		if ( empcheck == is_full_time )
-			empHrs = 8;
-		else if (empcheck==is_part_time)
-			empHrs = 4;
-		else
-			empHrs = 0;
-		    empwage = empHrs * Employee_wage_per_Hour;
-		    System.out.println("empwage: " + empwage);
-	}
+		public static final int is_full_time = 1;
+		public static final int is_part_time = 2;
+		public static final int Employee_wage_per_Hour = 20;
+		
+		public static void main(String[] args) {
+			
+			int empHrs = 0;
+			int empwage = 0;
+		
+			int empcheck = (int) Math.floor(Math.random() * 100) % 3;
+			switch (empcheck) {
+			
+				case is_full_time:
+					empHrs = 8;
+					break;
+			
+				case is_part_time:
+			    	empHrs = 4;
+			    	break;
+			
+				default:
+					empHrs = 0;
+					break;
+				}
+			empwage = empHrs * Employee_wage_per_Hour;
+			    System.out.println("empwage: " + empwage);
+			    		
+			}
 }

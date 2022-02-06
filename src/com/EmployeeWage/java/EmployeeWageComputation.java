0a71430bba7ae40fce421941186cfaa90c7440b0
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-06 23:52:04 +0530

    Added : Calculating Employee 20 days Working day per mouth

--- src/com/EmployeeWage/java/EmployeeWageComputation.java
+++ src/com/EmployeeWage/java/EmployeeWageComputation.java
@@ -4,13 +4,16 @@ public class EmployeeWageComputation {
 		public static final int is_full_time = 1;
 		public static final int is_part_time = 2;
 		public static final int Employee_wage_per_Hour = 20;
+		public static final int number_of_working_day= 20;
 		
 		public static void main(String[] args) {
 			
 			int empHrs = 0;
 			int empwage = 0;
-		
-			int empcheck = (int) Math.floor(Math.random() * 100) % 3;
+			int totalempwage = 0;
+			
+			for (int day = 0; day < number_of_working_day; day++) {
+			int empcheck = (int) Math.floor(Math.random()*100) % 3;
 			switch (empcheck) {
 			
 				case is_full_time:
@@ -23,10 +26,14 @@ public class EmployeeWageComputation {
 			
 				default:
 					empHrs = 0;
-					break;
 				}
 			empwage = empHrs * Employee_wage_per_Hour;
 			    System.out.println("empwage: " + empwage);
-			    		
 			}
+			System.out.println("Total Emp Wage: " + totalempwage );
+			
+		}
+			
 }
+
+

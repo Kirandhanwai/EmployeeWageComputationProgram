d33f37c73c35ce3e97598fe09b87a921ba17a2da
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-06 22:44:14 +0530

    Added : Employee Present or Absent

--- src/com/EmployeeWage/java/EmployeeWageComputation.java
+++ src/com/EmployeeWage/java/EmployeeWageComputation.java
@@ -2,8 +2,12 @@ package com.EmployeeWage.java;
 
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
-		
-		
-	
+		int is_full_time = 1;
+		double empcheck = Math.floor(Math.random() * 100) % 2;
+	if ( empcheck == is_full_time )
+				System.out.println("Employee is Presnet");
+	else
+				System.out.println("Employee is Absent");
+			
 	}
 }

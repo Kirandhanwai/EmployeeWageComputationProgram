be4c9c3325f232aff2dc28c1a852c8dbe3f58319
Kirandhanwai <97426847+Kirandhanwai@users.noreply.github.com>
2022-02-07 20:16:04 +0530

    Update EmployeeWageComputation.java

--- src/com/EmployeeWage/java/EmployeeWageComputation.java
+++ src/com/EmployeeWage/java/EmployeeWageComputation.java
@@ -1,40 +1,6 @@
 package com.EmployeeWage.java;
 
 public class EmployeeWageComputation {
-<<<<<<< HEAD
-		public static final int is_full_time = 1;
-		public static final int is_part_time = 2;
-		public static final int Employee_wage_per_Hour = 20;
-		public static final int number_of_working_day= 20;
-		
-		public static void main(String[] args) {
-			
-			int empHrs = 0;
-			int empwage = 0;
-			int totalempwage = 0;
-			
-			for (int day = 0; day < number_of_working_day; day++) {
-			int empcheck = (int) Math.floor(Math.random()*100) % 3;
-			switch (empcheck) {
-			
-				case is_full_time:
-					empHrs = 8;
-					break;
-			
-				case is_part_time:
-			    	empHrs = 4;
-			    	break;
-			
-				default:
-					empHrs = 0;
-				}
-			empwage = empHrs * Employee_wage_per_Hour;
-			    System.out.println("empwage: " + empwage);
-			}
-			System.out.println("Total Emp Wage: " + totalempwage );
-			
-		}
-=======
 	public static final int is_full_time = 1;
 	public static final int is_part_time = 2;
 	public static final int Employee_wage_per_Hour = 20;
@@ -65,7 +31,7 @@ public class EmployeeWageComputation {
 		
 			default:
 				empHrs = 0;
-				break ;
+				break  ;
 	    }
 		totalemphrs += empHrs;
 		empwage = empHrs * Employee_wage_per_Hour;
@@ -75,8 +41,6 @@ public class EmployeeWageComputation {
 		System.out.println("Total Emp Wage: " + totalempwage );
 		    		
 	}
-
 			
 }
-
 
